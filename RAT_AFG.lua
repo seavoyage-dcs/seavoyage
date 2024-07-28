2d99481f72e5d46db3d00e390e8f54a5eb1c4347
seavoyage-dcs <seavoyage@live.com>
2024-07-28 12:58:33 +0200

    Update RAT_AFG.lua

--- RAT_AFG.lua
+++ RAT_AFG.lua
@@ -11,7 +11,7 @@
 -- # Test cases:
 --
 -- 1. A-10C are spawned at Kandahar with desitation Camp Bastion, Dwyer or Shinhad.
--- 2. V-22 are spawned at Camp Bastion with desitation Kandahar, Dwyer or Shindad.
+-- 2. CH-47 are spawned at Camp Bastion with desitation Kandahar, Dwyer or Shindad.
 -- 3. F-18C are spawned at Kandahar with desitation Camp Bastion.
 -- 4. KC-130 are spawned in air at two zones heading for Kandahar.
 
@@ -32,28 +32,28 @@ a10:Spawn(3)
 
 -- -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-local v22=RAT:New("RAT_V22")
+local ch47=RAT:New("RAT_CH47")
 
--- Departure Kandahar.
-v22:SetDeparture(AIRBASE.Afghanistan.Camp Bastion)
-v22:SetCoalition("same")
+-- Departure Camp Bastion.
+ch47:SetDeparture(AIRBASE.Afghanistan.Camp Bastion)
+ch47:SetCoalition("same")
 
--- Destination Camp Bastion, Dwyer or Shinddad.
-v22:SetDestination({AIRBASE.Afghanistan.Kandahar, AIRBASE.Afghanistan.Dwyer, AIRBASE.Afghanistan.Shindad})
+-- Destination Kandahar, Dwyer or Shinddad.
+ch47:SetDestination({AIRBASE.Afghanistan.Kandahar, AIRBASE.Afghanistan.Dwyer, AIRBASE.Afghanistan.Shindad})
 
 -- Spawn three flights.
-v22:Spawn(3)
+ch47:Spawn(3)
 
 -- -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- Creat RAT object from F/A-18C template.
+-- Create RAT object from F/A-18C template.
 local f18=RAT:New("RAT_F18C")
 f18:SetCoalition("same")
 
 -- Departure airports.
 f18:SetDeparture(AIRBASE.Afghanistan.Kandahar)
 
--- Destination Nellis.
+-- Destination Camp Bastion.
 f18:SetDestination(AIRBASE.Afghanistan.Camp Bastion)
 
 -- Spawn two flights.
@@ -62,23 +62,20 @@ f18:Spawn(2)
 
 -- -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- Create RAT object from KC-130 template.
-local kc130=RAT:New("RAT_KC130")
+-- Create RAT object from KC-135 template.
+local kc135=RAT:New("RAT_KC135")
 
 -- Set departure zones. We need takeoff "air" for that.
-kc130:SetDeparture({"RAT Zone North", "RAT Zone East"})
+kc135:SetDeparture("RAT Zone East")
 
 -- Set spawn in air.
-kc130:SetTakeoff("air")
+kc135:SetTakeoff("air")
 
--- Aircraft will fly to Kandahar
-kc130:SetDestination(AIRBASE.Afghanistan.Kandahar)
+-- Aircraft will fly to Shindand
+kc135:SetDestination(AIRBASE.Afghanistan.Shindand)
 
--- Spawning of aircraft will happen in 2 minute intervalls.
-kc130:SetSpawnInterval(120)
-
--- Spawn two aircraft.
-kc135:Spawn(2)
+-- Spawn one aircraft.
+kc135:Spawn(1)
 
 -- -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
